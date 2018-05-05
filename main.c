b91d4e53fa5cfaa960d92e452c864d60a89dd502
Santiago Rojo <tiagox@gmail.com>
2018-05-05 11:28:12 -0300

    Encierro la resolución de la primera entrega en su propio _scope_

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define SUCCESS 0
+#define ERROR 1
+
 void print_help() {
 	printf("Generador de datos en formato JSON.\n\n");
 	printf("uso: untref-ayp3-tp [-f <nombre_archivo>]\n\n");
@@ -11,23 +14,28 @@ int main(int argc, char **argv) {
 
 	FILE* output_file = stdout;
 
-	if (argc > 1) {
-		if (argc == 3 && strcmp(argv[1], "-f") == 0 && strlen(argv[2]) > 0) {
-			output_file = fopen(argv[2], "w");
-			if (!output_file) {
-				printf(
-						"Ocurrió un error al tratar de crear o acceder al archivo especificado.\n");
-				return 1;
+	// Primera entrega 2018-05-05
+	{
+		if (argc > 1) {
+			if (argc == 3 && strcmp(argv[1], "-f") == 0
+					&& strlen(argv[2]) > 0) {
+				output_file = fopen(argv[2], "w");
+
+				if (!output_file) {
+					printf(
+							"Ocurrió un error al tratar de crear o acceder al archivo especificado.\n");
+					return ERROR;
+				}
+			} else {
+				print_help();
+				return ERROR;
 			}
-		} else {
-			print_help();
-			return 1;
 		}
+
+		fprintf(output_file, "{}\n");
+
+		fclose(output_file);
 	}
 
-	fprintf(output_file, "{}\n");
-
-	fclose(output_file);
-
-	return 0;
+	return SUCCESS;
 }
